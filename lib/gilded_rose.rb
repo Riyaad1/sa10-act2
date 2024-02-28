d1ea448f54efd24f2ff32013d32e29913acedf15
Riyaad <riyaadhowlader@gmail.com>
2024-02-27 19:39:27 -0600

    new normal class with tick method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -49,3 +49,18 @@ class GildedRose
     @quality += 1 if @days_remaining < 5
   end
 end
+
+class Normal
+  attr_reader :days_remaining, :quality
+
+  def initialize( days_remaining:, quality:)
+    @quality, @days_remaining = quality, days_remaining
+  end
+
+  def tick
+    @days_remaining -= 1
+    return if @quality == 0
+    @quality -= 1
+    @quality -= 1 if days_remaining <= 0
+  end
+end
